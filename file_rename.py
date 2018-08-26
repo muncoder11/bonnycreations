fec7ed90448c6fa11fe424a57c6a86bccd60ffd9
muncoder11 <rajarshighosh30@gmail.com>
2018-08-26 12:20:14 +0530

    update

---
+++ file_rename.py
@@ -0,0 +1,20 @@
+import os, glob, shutil
+from os import path
+import sys
+
+
+folder_name = sys.argv[1]
+all_files = glob.glob( folder_name + '/*.jpg')
+print(all_files)
+
+#
+
+for index, filename in enumerate(all_files):
+
+    # file rename
+    current_filename = path.realpath(filename)
+    print(current_filename)
+    new_filename = folder_name + '/' + str(index + 1) + '.jpg'
+    os.rename(current_filename, new_filename)
+
+    print('renamed {0} files'.format(index+1))
